45c044213d0af0535661b351336e6baa75ef3ef7
Steve Boyd <emteknetnz@gmail.com>
2023-02-01 18:58:47 +1300

    .

--- src/DeprecationDiffTask.php
+++ src/DeprecationDiffTask.php
@@ -39,6 +39,8 @@ class DeprecationDiffTask extends BuildTask
 
     private $changelog = [];
 
+    private $toDeprecateInCms4 = [];
+
     // hardcoded list of silverstripe/framework Email.php methods that no longer exists
     // on Email.php, are instead defined on parent class in symfony/email and have a different
     // param and/or return type signature
@@ -91,7 +93,7 @@ class DeprecationDiffTask extends BuildTask
                 }
                 $dir = "$vendorDir/$subdir";
                 if ($dir != '/var/www/vendor/silverstripe/framework') {
-                    // continue;
+                    //continue;
                 }
                 foreach ([
                     'src',
@@ -110,9 +112,16 @@ class DeprecationDiffTask extends BuildTask
                 $this->fileinfo = [];
             }
         }
-        echo "\n\nCHANGELOG:\n\n";
-        echo implode("\n", $this->changelog);
-        echo "\n\n";
+        $f = BASE_PATH . '/_output/_changelog.txt';
+        file_put_contents($f, implode("\n", $this->changelog));
+        echo "\n\nWrote to $f\n";
+        //
+        $f = BASE_PATH . '/_output/_to_deprecate_in_cms4.txt';
+        ob_start();
+        print_r($this->toDeprecateInCms4);
+        $s = ob_get_clean();
+        file_put_contents($f, $s);
+        echo "Wrote to $f\n\n";
     }
 
     public function output(string $dir)
@@ -199,40 +208,42 @@ class DeprecationDiffTask extends BuildTask
                     }
                 }
                 if ($k == 'methods') {
-                    for ($i = 0; $i < count($f['cms4'][$k][$name]['params'] ?? []); $i++) {
-                        // skip if the method doesn't exist in cms5
-                        if (!isset($f['cms5'][$k][$name])) {
-                            continue;
+                    foreach (array_keys($f['cms4'][$k]) as $name) {
+                        for ($i = 0; $i < count($f['cms4'][$k][$name]['params'] ?? []); $i++) {
+                            // skip if the method doesn't exist in cms5
+                            if (!isset($f['cms5'][$k][$name])) {
+                                continue;
+                            }
+                            $key = "$path--$type-$name.$i";
+                            $cms4_param_type = $f['cms4'][$k][$name]['params'][$i]['type'] ?? '';
+                            $cms4_param_name = $f['cms4'][$k][$name]['params'][$i]['name'] ?? '';
+                            $cms5_param_type = $f['cms5'][$k][$name]['params'][$i]['type'] ?? '';
+                            $cms5_param_name = $f['cms5'][$k][$name]['params'][$i]['name'] ?? '';
+                            if ($cms4_param_name != $cms5_param_name || $cms4_param_type != $cms5_param_type) {
+                                $paramsDiff[$key] = [
+                                    'type' => 'param',
+                                    'name' => "$name",
+                                    'class' => $f['cms4']['namespace'] . '\\' . $f['cms4']['name'],
+                                    'path' => $path,
+                                    'cms4_param_name' => $cms4_param_name,
+                                    'cms5_param_name' => $cms5_param_name,
+                                    'cms4_param_type' => $cms4_param_type,
+                                    'cms5_param_type' => $cms5_param_type,
+                                ];
+                            }
                         }
-                        $key = "$path--$type-$name.$i";
-                        $cms4_param_type = $f['cms4'][$k][$name]['params'][$i]['type'] ?? '';
-                        $cms4_param_name = $f['cms4'][$k][$name]['params'][$i]['name'] ?? '';
-                        $cms5_param_type = $f['cms5'][$k][$name]['params'][$i]['type'] ?? '';
-                        $cms5_param_name = $f['cms5'][$k][$name]['params'][$i]['name'] ?? '';
-                        if ($cms4_param_name != $cms5_param_name || $cms4_param_type != $cms5_param_type) {
-                            $paramsDiff[$key] = [
-                                'type' => 'param',
-                                'name' => "$name",
-                                'class' => $f['cms4']['namespace'] . '\\' . $f['cms4']['name'],
-                                'path' => $path,
-                                'cms4_param_name' => $cms4_param_name,
-                                'cms5_param_name' => $cms5_param_name,
-                                'cms4_param_type' => $cms4_param_type,
-                                'cms5_param_type' => $cms5_param_type,
-                            ];
-                        }
-                    }
-                    if (isset($f['cms4'][$k][$name]['returnType']) && isset($f['cms5'][$k][$name]['returnType'])) {
-                        if ($f['cms4'][$k][$name]['returnType'] != $f['cms5'][$k][$name]['returnType']) {
-                            $key = "$path--$type-$name.returnType";
-                            $returnTypesDiff[$key] = [
-                                'type' => 'returnType',
-                                'name' => $name,
-                                'class' => $f['cms4']['namespace'] . '\\' . $f['cms4']['name'],
-                                'path' => $path,
-                                'cms4_returnType' => $f['cms4'][$k][$name]['returnType'],
-                                'cms5_returnType' => $f['cms5'][$k][$name]['returnType'],
-                            ];
+                        if (isset($f['cms4'][$k][$name]['returnType']) && isset($f['cms5'][$k][$name]['returnType'])) {
+                            if ($f['cms4'][$k][$name]['returnType'] != $f['cms5'][$k][$name]['returnType']) {
+                                $key = "$path--$type-$name.returnType";
+                                $returnTypesDiff[$key] = [
+                                    'type' => 'returnType',
+                                    'name' => $name,
+                                    'class' => $f['cms4']['namespace'] . '\\' . $f['cms4']['name'],
+                                    'path' => $path,
+                                    'cms4_returnType' => $f['cms4'][$k][$name]['returnType'],
+                                    'cms5_returnType' => $f['cms5'][$k][$name]['returnType'],
+                                ];
+                            }
                         }
                     }
                 }
@@ -280,9 +291,11 @@ class DeprecationDiffTask extends BuildTask
             }
             $removedInCms5ButNotDeprecatedInCms4[$key] = $a;
         }
+        $toDeprecateInCms4 = $cleanThing($removedInCms5ButNotDeprecatedInCms4, $deprecatedInCms4);
         print_r([
-            'Removed in CMS 5 but not deprecated in CMS 4' => $cleanThing($removedInCms5ButNotDeprecatedInCms4, $deprecatedInCms4)
+            'Removed in CMS 5 but not deprecated in CMS 4' => $toDeprecateInCms4
         ]);
+        $this->toDeprecateInCms4 = array_merge($this->toDeprecateInCms4, ['', $dir,''], $toDeprecateInCms4);
         echo "\n===================\n";
         echo "\n\nRAW DATA\n\n";
         print_r([
@@ -299,7 +312,6 @@ class DeprecationDiffTask extends BuildTask
         file_put_contents($f, $s);
         echo "Wrote to $f\n";
 
-        ksort($removedInCms5);
         $depr = [];
         foreach ($removedInCms5 as $key => $a) {
             if (strpos($key, '--') === false) {
@@ -317,14 +329,16 @@ class DeprecationDiffTask extends BuildTask
                     continue;
                 }
                 if ($this->isFrameworkEmailMethod($key)) {
-                    $depr[] = "- Method `{$a['class']}::{$a['name']}()` is now defined in `Symfony\Component\Mime\Email` with a different method signature`";
+                    // note: the API docs DO NOT contain details for API inherited from vendor classes
+                    // e.g. https://api.silverstripe.org/5/SilverStripe/Dev/SapphireTest.html which extends
+                    // phpunit TestCase does NOT contain info about assertSame()
+                    $depr[] = "- Method {$this->getChangelogMethod($a)} is now defined in `Symfony\Component\Mime\Email` with a different method signature`";
                 } else {
-                    $depr[] = "- Removed deprecated method `{$a['class']}::{$a['name']}()`";
+                    $depr[] = "- Removed deprecated method {$this->getChangelogMethod($a)}}";
                 }
             }
         }
         $jointDiff = array_merge($paramsDiff, $returnTypesDiff);
-        ksort($jointDiff);
         foreach ($jointDiff as $key => $a) {
             $classKey = explode('--', $key)[0];
             if (isset($removedInCms5[$classKey])) {
@@ -334,20 +348,42 @@ class DeprecationDiffTask extends BuildTask
             if (str_contains($key, 'returnType')) {
                 $cms4_returnType = $a['cms4_returnType'] ?: $untyped;
                 $cms5_returnType = $a['cms5_returnType'] ?: $untyped;
-                $depr[] = "- Return type changed for `{$a['class']}::{$a['name']}()` from `$cms4_returnType` to `$cms5_returnType`";
+                $depr[] = "- Changed return type for {$this->getChangelogMethodApiLink($a)} from `$cms4_returnType` to `$cms5_returnType`";
             } else {
                 $cms4_param_name = $a['cms4_param_name'];
                 $cms5_param_name = $a['cms5_param_name'];
                 $cms4_param_type = $a['cms4_param_type'] ?: $untyped;
                 $cms5_param_type = $a['cms5_param_type'] ?: $untyped;
-                if ($cms4_param_name != $cms5_param_name) {
-                    $depr[] = "- Changed parameter name in `{$a['class']}::{$a['name']}()` from `\${$cms4_param_name}` to `\${$cms5_param_name}`";
-                }
-                if ($cms4_param_type != $cms5_param_type && $cms5_param_name) {
-                    $depr[] = "- Changed parameter type in `{$a['class']}::{$a['name']}()` for `\${$cms5_param_name}` from `{$cms4_param_type}` to `{$cms5_param_type}`";
+                if (empty($cms5_param_name)) {
+                    $depr[] = "- Removed parameter in {$this->getChangelogMethodApiLink($a)} named `\${$cms4_param_name}";
+                } else {
+                    if ($cms4_param_name != $cms5_param_name) {
+                        $depr[] = "- Changed parameter name in {$this->getChangelogMethodApiLink($a)} from `\${$cms4_param_name}` to `\${$cms5_param_name}`";
+                    }
+                    if ($cms4_param_type != $cms5_param_type) {
+                        $depr[] = "- Changed parameter type in {$this->getChangelogMethodApiLink($a)} for `\${$cms5_param_name}` from `{$cms4_param_type}` to `{$cms5_param_type}`";
+                    }
                 }
             }
         }
+        usort($depr, function($a, $b) {
+            // non api link
+            $rx = "#\`([A-Za-z0-9_]+\\\[^`]+)`#";
+            $rx2 = "#\[([A-Za-z0-9_]+\\\[^\]]+)#";
+            if (!preg_match($rx, $a, $ma)) {
+                if (!preg_match($rx2, $a, $ma)) {
+                    var_dump($a);
+                    die;
+                }
+            }
+            if (!preg_match($rx, $b, $mb)) {
+                if (!preg_match($rx2, $b, $mb)) {
+                    var_dump($b);
+                    die;
+                }
+            }
+            return strtolower($ma[1]) <=> strtolower($mb[1]);
+        });
         if (!empty($depr)) {
             $this->changelog[] = '';
             $this->changelog[] = '#### ' . $this->getComposerName($dir);
@@ -356,6 +392,16 @@ class DeprecationDiffTask extends BuildTask
                 $this->changelog[] = $line;
             }
         }
+    }
+
+    private function getChangelogMethodApiLink($a)
+    {
+        return "[{$a['class']}::{$a['name']}()](api:{$a['class']}::{$a['name']}())";
+    }
+
+    private function getChangelogMethod($a)
+    {
+        return "`{$a['class']}::{$a['name']}()`";
     }
 
     private function isFrameworkEmailMethod($key)
